e53236cf292d93f14e3f76a2d78f099929001946
flues <135176290+fluesdev@users.noreply.github.com>
2024-11-11 12:18:21 +0100

    Updated Version 1.1
    -Added new difficult levels!
    -Improved code
    -Improved text

--- zgadywanka.py
+++ zgadywanka.py
@@ -3,24 +3,65 @@
 import random
 
 secret_number = random.randint(1, 100)
-max_attemps = 10 
+easy_attemps = 10
+medium_attemps = 7
+hard_attemps = 3 
 attemp = 0 
 
-
-print("Hello to my game try to guess number that i picked in my mind")
-
-
-while attemp < max_attemps:
-    guess = int(input("Try to guess: "))
-    attemp += 1 
-
-    if guess < secret_number:
-        print("Your number is lower than secret number")
-    elif guess > secret_number:
-        print("Your number is higher than my secret number")
+print("Welcome to SecretGame!")
+while True:
+    diff = int(input("Easy <- 1 \nMedium <- 2 \nHard <-3\nSo what's your pick: "))
+    if diff in [1, 2, 3]:
+        break 
     else:
-        print(f"Good! job your guesses my secret number in {attemp}!")
-        break
-else:
-    print(f"You didn't guess my number in max number attemps")
+        print("Wrong number, try again.")
 
+    
+    
+#input for user to change difficult lvl 
+#easy 10 tries
+#medium 7 tries 
+#hard 3 tries
+if diff == 1:
+    while attemp < easy_attemps:
+        guess = int(input("Try to guess: "))
+        attemp += 1 
+
+        if guess < secret_number:
+            print("Your number is lower than secret number")
+        elif guess > secret_number:
+            print("Your number is higher than my secret number")
+        else:
+            print(f"Good job! you guessed my secret number in {attemp}!")
+            break
+    else:
+        print(f"You didn't guess my number in max number attemps")
+
+if diff == 2:
+    while attemp < medium_attemps:
+        guess = int(input("Try to guess: "))
+        attemp += 1 
+
+        if guess < secret_number:
+            print("Your number is lower than secret number")
+        elif guess > secret_number:
+            print("Your number is higher than my secret number")
+        else:
+            print(f"Good job! you guessed my secret number in {attemp}!")
+            break
+    else:
+        print(f"You didn't guess my number in max number attemps")
+if diff == 3:
+    while attemp < hard_attemps:
+        guess = int(input("Try to guess: "))
+        attemp += 1 
+
+        if guess < secret_number:
+            print("Your number is lower than secret number")
+        elif guess > secret_number:
+            print("Your number is higher than my secret number")
+        else:
+            print(f"Good job! you guessed my secret number in {attemp}!")
+            break
+    else:
+        print(f"You didn't guess my number in limited tries")
